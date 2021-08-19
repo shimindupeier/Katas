33b69bddac1dfe6583455a3f88b14a0e93faf70c
Jasmine Dupre <jasmine.dupre@ymail.com>
2021-08-19 14:52:03 +0100

    Kata7 Codewars initial commit with Kotlin 1.5 and test

---
+++ src/main/kotlin/Kata7.kt
@@ -0,0 +1,19 @@
+import kotlin.math.pow
+
+fun main() {
+    println(revRot("73304991087281576455176044327690580265896896028", 8))
+}
+
+fun revRot(nums: String, sz: Int): String {
+    // return empty string if string length is less than sz or empty
+    if (sz <= 0 || sz > nums.length) return ""
+
+    // The code below doesn't work with Codewars. Here we use kotlin 1.5 while codewars supports only 1.3
+    return nums.chunked(sz).filter { i -> i.length == sz }.joinToString("") { i ->
+        if ((i.sumOf { it.digitToInt().toDouble().pow(3.0)}) % 2.0 == 0.0) {
+            i.reversed()
+        } else {
+            i.drop(1) + i.take(1)
+        }
+    }
+}
---
+++ src/test/kotlin/Kata7KtTest.kt
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class Kata7KtTest {
+    private fun testing(actual:String, expected:String) {
+        assertEquals(expected, actual)
+    }
+    @Test
+    fun test1() {
+        println("Fixed Tests: revRot")
+        var s = "733049910872815764"
+        testing(revRot(s, 5), "330479108928157")
+        s = "73304991087281576455176044327690580265896"
+        testing(revRot(s, 8), "1994033775182780067155464327690480265895")
+        s = "73304991087281576455176044327690580265896896028"
+        testing(revRot(s, 8), "1994033775182780067155464327690480265895")
+
+    }
+}
